d7d91282b3a8640e407d760c327c8196994f68aa
eylens <eymasac@gmail.com>
2021-12-01 23:09:58 -0500

    the code is reused for the first commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,6 +9,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
+
 	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
@@ -37,6 +38,51 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_balance - This function measures the balance factor of
+ * a binary tree
+ * @tree:  is a pointer to the root node of the tree to measure
+ * the balance factor
+ * Return: the difference between the heights of the right and
+ * left tree.
+ * If tree is NULL, return 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t hight_left, hight_right;
+
+	if (tree == NULL)
+		return (0);
+
+	hight_left = binary_tree_height(tree->left);
+	if (tree->left == NULL)
+		hight_left--;
+	hight_right = binary_tree_height(tree->right);
+	if (tree->right == NULL)
+		hight_right--;
+
+	return (hight_left - hight_right);
+}
+
+/**
+ * binary_tree_is_full - This function checks if a BTree is full
+ * @tree:  is a pointer to the root node of the tree to check
+ * Return: 1 if is full, otherwise return 0
+ * If tree is NULL return 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
+
+/**
  * binary_tree_is_perfect - This function checks if a node is a leaf
  * A Perfect BTree: all internal nodes have two children and
  * all leaf nodes have the same level.
@@ -45,24 +91,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	binary_tree_t *izq, *der;
-
-	izq = tree->left;
-	der = tree->right;
-
-	if (izq == NULL || der == NULL)
-		return (0);
-
 	if (tree == NULL)
 		return (0);
-
-	if (binary_tree_is_leaf(tree))
+	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
 		return (1);
-
-	if (binary_tree_height(izq) == binary_tree_height(der))
-	{
-		if (binary_tree_is_perfect(izq) && binary_tree_is_perfect(der))
-			return (1);
-	}
 	return (0);
 }
