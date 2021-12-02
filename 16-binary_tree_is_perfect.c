ef81d1b632e2f7fe4dad1b56c3cee633d5943ff8
eylens <eymasac@gmail.com>
2021-12-02 11:27:08 -0500

    evaluate child pointers

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -91,9 +91,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (tree == NULL || tree->left == NULL || tree->right == NULL)
 		return (0);
-	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
+	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1 && binary_tree_height)
 		return (1);
 	return (0);
 }
