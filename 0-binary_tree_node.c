e46fac1e05c9a1d724f0eeffcb2de246aa7a8804
eylens <eymasac@gmail.com>
2021-12-01 22:41:01 -0500

    the code was separate to create the root, and if the root already existed and create another node. Now it remains general

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,29 +10,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	/* root node */
-	if (parent == NULL)
-	{
-		new = malloc(sizeof(binary_tree_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = value;
-		new->left = NULL;
-		new->right = NULL;
-		parent = new;
-	}
-
-	/* root node exists, creating & adding new node to parent node */
-	else
-	{
-		new = malloc(sizeof(binary_tree_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = value;
-		new->left = NULL;
-		new->right = NULL;
-		new->parent = parent;
-	}
-
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 	return (new);
 }
