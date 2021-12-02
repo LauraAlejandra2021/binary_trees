e90939ea1211aa3e296512f6c920b8c5b7e7eabf
eylens <eymasac@gmail.com>
2021-12-01 22:30:20 -0500

    description 4 function adjusted

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - This function finds the uncle of a node
+ * binary_tree_uncle - This function finds the uncle of a node
  * @node: is a pointer to the node to find the uncle
  * Return: a pointer to the uncle node
  * If node is NULL, return NULL
